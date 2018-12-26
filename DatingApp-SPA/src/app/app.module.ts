8ae97280476a4b3cda388727d347c4f43ad73f42
qishichang <qishichang@live.com>
2018-12-26 17:08:07 +1300

    Add Angular JWT, NGX Bootstrap, and Bootswatch

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,8 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BsDropdownModule } from 'ngx-bootstrap';
+
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
@@ -23,7 +24,8 @@ import { AlertifyService } from './_services/alertify.service';
       BrowserModule,
       HttpClientModule,
       FormsModule,
-      AppRoutingModule
+      AppRoutingModule,
+      BsDropdownModule.forRoot()
    ],
    providers: [
       AuthService,
