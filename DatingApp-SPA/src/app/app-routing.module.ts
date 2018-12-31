270a8005a3f3ba129bab6a43dd00f82e15ba7e94
qishichang <qishichang@live.com>
2018-12-31 16:32:13 +1300

    Add CanDeactive guard

--- DatingApp-SPA/src/app/app-routing.module.ts
+++ DatingApp-SPA/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -20,7 +21,8 @@ const routes: Routes = [
     children: [
       { path: 'members', component: MemberListComponent, resolve: { users: MemberListResolver } },
       { path: 'members/:id', component: MemberDetailComponent, resolve: { user: MemberDetailResolver } },
-      { path: 'member/edit', component: MemberEditComponent, resolve: { user: MemberEditResolver } },
+      { path: 'member/edit', component: MemberEditComponent, resolve: { user: MemberEditResolver },
+          canDeactivate: [ PreventUnsavedChanges ] },
       { path: 'messages', component: MessagesComponent },
       { path: 'lists', component: ListsComponent }
     ]
@@ -33,6 +35,7 @@ const routes: Routes = [
   exports: [RouterModule],
   providers: [
     AuthGuard,
+    PreventUnsavedChanges,
     MemberDetailResolver,
     MemberListResolver,
     MemberEditResolver,
