611a6d629253452b070b8b38b7939bcec88cc66c
qishichang <qishichang@live.com>
2019-01-09 10:32:39 +1300

    Add Inbox and Outbox

--- DatingApp-SPA/src/app/app-routing.module.ts
+++ DatingApp-SPA/src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { ListsResolver } from './_resolvers/lists.resolver';
+import { MessagesResolver } from './_resolvers/messages.resolver';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -24,7 +25,7 @@ const routes: Routes = [
       { path: 'members/:id', component: MemberDetailComponent, resolve: { user: MemberDetailResolver } },
       { path: 'member/edit', component: MemberEditComponent, resolve: { user: MemberEditResolver },
           canDeactivate: [ PreventUnsavedChanges ] },
-      { path: 'messages', component: MessagesComponent },
+      { path: 'messages', component: MessagesComponent, resolve: { messages: MessagesResolver } },
       { path: 'lists', component: ListsComponent, resolve: { users: ListsResolver } }
     ]
   },
@@ -41,6 +42,7 @@ const routes: Routes = [
     MemberListResolver,
     MemberEditResolver,
     ListsResolver,
+    MessagesResolver,
   ]
 })
 export class AppRoutingModule { }
