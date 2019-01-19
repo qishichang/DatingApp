6e920fb79f12c7e4bb593669d8dfcf021bb2730d
qishichang <qishichang@live.com>
2019-01-19 22:14:21 +1300

    Add edit roles modal

--- DatingApp-SPA/src/app/admin/user-management/user-management.component.ts
+++ DatingApp-SPA/src/app/admin/user-management/user-management.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { User } from '../../_models/user';
 import { AdminService } from '../../_services/admin.service';
 import { AlertifyService } from '../../_services/alertify.service';
+import { RolesModalComponent } from '../roles-modal/roles-modal.component';
 
 @Component({
   selector: 'app-user-management',
@@ -10,8 +12,10 @@ import { AlertifyService } from '../../_services/alertify.service';
 })
 export class UserManagementComponent implements OnInit {
   users: User[];
+  bsModalRef: BsModalRef;
 
   constructor(private adminService: AdminService,
+    private modalService: BsModalService,
     private alertify: AlertifyService) { }
 
   ngOnInit() {
@@ -24,5 +28,55 @@ export class UserManagementComponent implements OnInit {
     }, error => {
       this.alertify.error(error);
     });
+  }
+
+  editRolesModal(user: User) {
+    const initialState = {
+      user,
+      roles: this.getRolesArray(user)
+    };
+    this.bsModalRef = this.modalService.show(RolesModalComponent, {initialState});
+    this.bsModalRef.content.updateSelectedRoles.subscribe((values) => {
+      const rolesToUpdate = {
+        roleNames: [...values.filter(el => el.checked === true).map(el => el.name)]
+      };
+      if (rolesToUpdate) {
+        this.adminService.updateUserRoles(user, rolesToUpdate).subscribe(() => {
+          user.roles = [...rolesToUpdate.roleNames];
+        }, error => {
+          this.alertify.error(error);
+        });
+      }
+    });
+  }
+
+  private getRolesArray(user: User) {
+    const roles = [];
+    const userRoles = user.roles;
+    const availableRoles: any[] = [
+      { name: 'Admin', value: 'Admin' },
+      { name: 'Moderator', value: 'Moderator' },
+      { name: 'Member', value: 'Member' },
+      { name: 'VIP', value: 'VIP' },
+    ];
+
+    for (let i = 0; i < availableRoles.length; i++) {
+      let isMatch = false;
+      for (let j = 0; j < userRoles.length; j++) {
+        if (availableRoles[i].name === userRoles[j]) {
+          isMatch = true;
+          availableRoles[i].checked = true;
+          roles.push(availableRoles[i]);
+          break;
+        }
+      }
+
+      if (!isMatch) {
+        availableRoles[i].checked = false;
+        roles.push(availableRoles[i]);
+      }
+    }
+
+    return roles;
   }
 }
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -3,33 +3,35 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { BsDropdownModule, TabsModule, BsDatepickerModule, PaginationModule, ButtonsModule } from 'ngx-bootstrap';
+import { BsDropdownModule, TabsModule, BsDatepickerModule, PaginationModule, ButtonsModule, ModalModule } from 'ngx-bootstrap';
 import { JwtModule } from '@auth0/angular-jwt';
 import { NgxGalleryModule } from 'ngx-gallery';
 import { FileUploadModule } from 'ng2-file-upload';
 import { TimeAgoPipe } from 'time-ago-pipe';
 
-import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
+import { HasRoleDirective } from './_directives/hasRole.directive';
 import { AuthService } from './_services/auth.service';
-import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { AlertifyService } from './_services/alertify.service';
+import { UserService } from './_services/user.service';
+import { AdminService } from './_services/admin.service';
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import { UserService } from './_services/user.service';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
 import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
-import { HasRoleDirective } from './_directives/hasRole.directive';
 import { UserManagementComponent } from './admin/user-management/user-management.component';
 import { PhotoManagementComponent } from './admin/photo-management/photo-management.component';
-import { AdminService } from './_services/admin.service';
+import { RolesModalComponent } from './admin/roles-modal/roles-modal.component';
 
 export function tokenGetter() {
    return localStorage.getItem('token');
@@ -54,6 +56,7 @@ export function tokenGetter() {
       HasRoleDirective,
       UserManagementComponent,
       PhotoManagementComponent,
+      RolesModalComponent,
    ],
    imports: [
       BrowserModule,
@@ -66,6 +69,7 @@ export function tokenGetter() {
       PaginationModule.forRoot(),
       TabsModule.forRoot(),
       ButtonsModule.forRoot(),
+      ModalModule.forRoot(),
       NgxGalleryModule,
       FileUploadModule,
       JwtModule.forRoot({
@@ -82,6 +86,9 @@ export function tokenGetter() {
       AlertifyService,
       ErrorInterceptorProvider,
       AdminService,
+   ],
+   entryComponents: [
+      RolesModalComponent,
    ],
    bootstrap: [
       AppComponent
